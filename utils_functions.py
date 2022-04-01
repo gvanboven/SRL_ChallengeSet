761b89ef7809da9514287643e5dd167002446bff
G van Boven <goya.vanboven@student.uva.nl>
2022-04-01 16:31:21 +0200

    documentation added

--- utils_functions.py
+++ utils_functions.py
@@ -10,8 +10,20 @@ import json
 ########## CORE  FUNCTIONALITIES ##########
 ###########################################
 
-# Helper function to extract target argument
 def get_arg(pred, verb_number, arg_target='ARG1'):
+    """ 
+    Helper function to extract target argument
+    :param pred: the model prediction
+    :param verm_number: the verb number of the predicate of interest
+    :param arg_target: the name of the argument of interest
+    :type pred: dict
+    :type verb_number: int
+    :type arg_target: string
+
+    :returns arg_set: the set of tokens that are classified to be this argument
+    :type arg_set: set
+
+    """
     # we assume one predicate:
     predicate_arguments = pred['verbs'][verb_number]
     words = pred['words']
@@ -25,17 +37,59 @@ def get_arg(pred, verb_number, arg_target='ARG1'):
     arg_set = set(arg_list)
     return arg_set
 
+
 def get_model_results(model, n, sentence, capability, testcase_name):
+    """
+    Extracts the predictions of a given model for a given sentence
+    And returns a dict with this info, which is to be stored in an output file
+
+    :param model: a pretrained srl model
+    :param n: the number of the current data sample
+    :param sentence: the current data sample sentence
+    :param capability: the name of the linguistic capability of interest
+    :param testcase_name: the name of the current test
+    :type model : allennlp_models.structured_prediction.predictors.srl
+    :type n: int
+    :type sentence: string
+    :type capability: string
+    :type testcase_name: string
+
+    :returns results: dict with all relevant info to store in output file
+    :type results: dict
+    """
     results = {'sentence' : sentence,
                'capability' : capability,
                'testcase_name' : testcase_name,
                'preds' : model.results['preds'][n],
                'confs': model.results['confs'][n],
-               #'expect_results' : model.results['expect_results'][n],
                'passed' : bool(model.results['passed'][n])}
     return results
 
 def extract_data_and_predictions(t, capability, testcase_name, test_srl, test_srlbert, test_data, SRL_predictions, SRLBERT_predictions):
+    """ 
+    Function that extracts all relevant test data and predictions information that is to be stored in the output files
+
+    :param t: dict containing all test case information
+    :param capability: the name of the linguistic capability of interest
+    :param testcase_name: the name of the current test
+    :param test_srl: predictions for the first SRL model
+    :param test_srlbert: predictions for the second SRL model
+    :param test_data: output list in which all test cases should be stored
+    :param SRL_predictions: output list in which all predictions of the first model should be stored
+    :param SRLBERT_predictions: output list in which all predictions of the second model should be stored
+    :type t: dict
+    :type capability: string
+    :type testcase_name: string
+    :type test_srl: dict
+    :type test_srlbert: dict
+    :type test_data: list
+    :type SRL_predictions: list
+    :type SRLBERT_predictions: list
+
+    :returns test_data: output list to which new test cases are added
+    :returns SRL_predictions: output list to which new predictions of the first model are added
+    :returns SRLBERT_predictions: output list to which new predictions of the second model are added
+    """
     for n, sentence in enumerate(t['data']):
         #extract input sentence info
         input_item = {'sentence' : sentence,
@@ -53,6 +107,34 @@ def extract_data_and_predictions(t, capability, testcase_name, test_srl, test_sr
 
 
 def predict_and_store(t, capability, testcase_name, expect, formattype, predict_srl, predict_srlbert, test_data, SRL_predictions, SRLBERT_predictions):
+    """ 
+    Function that creates test cases given a template, makes predictions for the given models and stores the test cases as well as the predictions
+
+    :param t: dict containing all test case information
+    :param capability: the name of the linguistic capability of interest
+    :param testcase_name: the name of the current test
+    :param expect: function that checks if the argument of interest is predicted as expected
+    :param formattype: function that creates the correct formatting for the test
+    :param predict_srl: first pretrained SRL model to be tested
+    :param predict_srlbert: second pretrained SRL model to be tested
+    :param test_data: output list in which all test cases should be stored
+    :param SRL_predictions: output list in which all predictions of the first model should be stored
+    :param SRLBERT_predictions: output list in which all predictions of the second model should be stored
+    :type t: dict
+    :type capability: string
+    :type testcase_name: string
+    :type expect: python function
+    :type formattype: python function
+    :type predict_srl: allennlp_models.structured_prediction.predictors.srl
+    :type predict_srlbert: allennlp_models.structured_prediction.predictors.srl
+    :type test_data: list
+    :type SRL_predictions: list
+    :type SRLBERT_predictions: list
+
+    :returns test_data: output list to which new test cases are added
+    :returns SRL_predictions: output list to which new predictions of the first model are added
+    :returns SRLBERT_predictions: output list to which new predictions of the second model are added
+    """
     #test the srl model
     print('SRL')
     test_srl = MFT(**t, expect=expect)
@@ -71,6 +153,16 @@ def predict_and_store(t, capability, testcase_name, expect, formattype, predict_
     return test_data, SRL_predictions, SRLBERT_predictions
 
 def store_data(path, data, new_file=True):
+    """
+    Function that saves a given list to a json file on the given path
+
+    :param path: path to a .json file to store data in
+    :param data: list containing information to be stored
+    :param new_file: setting indicating whether previous information in the file should be deleted (True) or not (False)
+    :type path: string
+    :type data: list
+    :type new_file: boolean
+    """
     #if there already is content in the file, make sure we do not lose it. 
     if new_file == False:
         with open(path, "r") as file:
@@ -88,19 +180,21 @@ def store_data(path, data, new_file=True):
 ###########################################
 
 
-# Helper function to display failures
+# Helper function to display failures for the first identified verb
 def format_srl_verb0(x, pred, conf, label=None, meta=None):
 
     predicate_structure = pred['verbs'][0]['description']
         
     return predicate_structure
 
+# Helper function to display failures for the second identified verb
 def format_srl_verb1(x, pred, conf, label=None, meta=None):
 
     predicate_structure = pred['verbs'][1]['description']
         
     return predicate_structure
 
+# Helper function to display failures for the third identified verb
 def format_srl_verb2(x, pred, conf, label=None, meta=None):
 
     predicate_structure = pred['verbs'][2]['description']
@@ -110,6 +204,8 @@ def format_srl_verb2(x, pred, conf, label=None, meta=None):
 ###########################################
 ########## ARGUMENT RECOGNITION ###########
 ###########################################
+
+# The functions below are used to control whether the correct tokens are identified as the arguments of interest
 
 ######### PATIENT RECOGNITION #############
 
